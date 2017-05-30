b8bf1e00f1a8dbb6232eb7e071af93aaceaf0aa1
Saran Pinpan <saran.pi@hotmail.com>
2017-05-30 19:01:52 +0700

    Initial Commit

---
+++ Cat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cat
+//
+//  Created by Saran Pinpan on 5/30/17.
+//  Copyright © 2017 com.saran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
